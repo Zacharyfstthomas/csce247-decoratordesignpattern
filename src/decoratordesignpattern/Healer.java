128d2aea09401bf893cff8e77bcac5b0e53f175f
Zachary St. Thomas <zfs@email.sc.edu>
2020-09-13 18:38:05 -0400

    Update Healer.java

--- src/decoratordesignpattern/Healer.java
+++ src/decoratordesignpattern/Healer.java
@@ -1,16 +1,12 @@
 package decoratordesignpattern;
 /**
- * 
- * 
  * Healer is one of the three concrete classes that inherit from the Player class.
  * Like the other two, it assigns values to the parent's variables, name, intellect,
  * attack, defense, weapon, and armor.
  * the parent's toString is also appended with the class name.
  * @author zacharystthomas
- *
  */
 public class Healer extends Player{
-
 	/**
 	 * Constructor. Sets the values of the abstract parent Player's variables.
 	 * @param name: the parent's name variable is set to this parameter.
