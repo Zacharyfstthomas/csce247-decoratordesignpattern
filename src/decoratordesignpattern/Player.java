8c24e99a38d8f3f95806c381576b146eed09cfd2
Zachary St. Thomas <zfs@email.sc.edu>
2020-09-13 18:42:14 -0400

    Update Player.java

--- src/decoratordesignpattern/Player.java
+++ src/decoratordesignpattern/Player.java
@@ -1,7 +1,5 @@
 package decoratordesignpattern;
 /**
- * 
- * 
  * Player is an Abstract class that is meant to store the shared variables between the three  
  * concrete Mage, Warrior and Healer classes, which inherit from this class, as well as both calculate the base power level for the class, and 
  * return the generic part of the message for a player (ex. carries a ___, wears a ___)
@@ -9,18 +7,14 @@ package decoratordesignpattern;
  * intellect,defense,and attack all are integers determined by which child concrete class is object is.
  * weapon and staff are both Strings determined by which child concrete class the object is.
  * @author zacharystthomas
- *
  */
 public abstract class Player {
-
 	protected String name;
 	protected int intellect;
 	protected int defense;
 	protected int attack;
 	protected String weapon;
 	protected String armor;
-	
-	
 	/**
 	 * 
 	 * Returns the power for any player, calculated by multiplying the attack value by three,  
@@ -28,24 +22,16 @@ public abstract class Player {
 	 * @return the result of the above calculation.
 	 */
 	public double getPower() {
-	
 	double power = (this.attack)*3 + this.defense + (this.intellect)/2;
-	return power;
-		
-	}
-	
+	return power;	
+	}	
 	/**
 	 * @return Returns the name value, followed by a new line 
 	 * and then the weapon value and the armor value 
 	 * framed as the equipment of the player.
-	 * 
-	 * 
 	 */
 	public String toString() {
-		
 		return this.name+"\nCarries a "+this.weapon+ ", and wears a "+ this.armor;
-	
-		
 	}
 	
 	
