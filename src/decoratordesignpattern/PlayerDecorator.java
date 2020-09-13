d25fe6b6a2f8479e5479516f813f7123db0b719d
Zachary St. Thomas <zfs@email.sc.edu>
2020-09-13 18:45:51 -0400

    Update PlayerDecorator.java

--- src/decoratordesignpattern/PlayerDecorator.java
+++ src/decoratordesignpattern/PlayerDecorator.java
@@ -1,16 +1,12 @@
 package decoratordesignpattern;
 /**
- * 
  * PlayerDecorator is a Abstract class which contains abstract methods. These methods are defined he,
  * to be used in the children of this class.
  * This class is also the child of Player, but because it is abstract, 
  * it does not need to implement its methods.
  * @author zacharystthomas
- *
  */
 public abstract class PlayerDecorator extends Player{
-
 	public abstract String toString();
 	public abstract double getPower();
-	 
 }
